ab22d22153dcf8d30683bfa2829ef221ca54f7cd
CHRadhaKalyani <chradha3514@gmail.com>
2024-02-25 14:45:50 +0530

    Added JaggedArray.java

---
+++ src/JaggedArray.java
@@ -0,0 +1,26 @@
+package src;
+
+public class JaggedArray {
+
+        public static void main(String args[]) {
+            int nums[][] = new int[3][]; // jagged
+
+            nums[0] = new int[4];
+            nums[1] = new int[2];
+            nums[2] = new int[3];
+
+            for (int i = 0; i < nums.length; i++) {
+                for (int j = 0; j < nums[i].length; j++) {
+
+                    nums[i][j] = (int) (Math.random() * 10);
+
+                }
+            }
+            // loop through each row of the jagged array
+            for (int i = 0; i < nums.length; i++) {
+                // loop through each column of the current row
+                for (int j = 0; j < nums[i].length; j++) {
+                    System.out.println(nums[i][j]);
+                }
+            }
+        }}
