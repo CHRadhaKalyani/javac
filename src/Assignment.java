c1e7cfe72962b87e28a6f5c1aed929b4a149300a
CHRadhaKalyani <chradha3514@gmail.com>
2024-03-04 19:51:33 +0530

    Assignment Operators

---
+++ src/Assignment.java
@@ -0,0 +1,33 @@
+package src;
+
+public class Assignment
+{
+    public static void main(String[] args)
+    {
+        int a = 5;
+        int b = 5;
+        int c = 5;
+        int d = 5;
+        int e = 5;
+        int f = 5;
+        int g = 5;
+        int h = 5;
+        int i = 5;
+        int j = 5;
+        a += 3;
+        b -= 3;
+        c *= 3;
+        d /= 3;
+        e %= 3;
+        i >>= 3;
+        j <<= 3;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(i);
+        System.out.println(j);
+
+    }
+}
